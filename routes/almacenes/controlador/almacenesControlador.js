d2a2fd946b6fbd881bc34bad752ab3984b0c21f5
JLGallardoV <Gallardo.vaca@gmail.com>
2019-09-19 12:21:38 -0500

    APIs ALMACENES - CONCLUIDO
    -No JWT
    -HMVC

---
+++ routes/almacenes/controlador/almacenesControlador.js
@@ -0,0 +1,76 @@
+var express = require('express');
+var router = express.Router();
+var almacenesModelo = require('../modelo/almacenesModelo');
+
+
+// LISTAR POPULARIDAD PRODUCTOS - EXPORTANDO RUTA
+router.get('/listarAlmacenes', function(req, res, next) {
+  try {
+    //web service
+    almacenesModelo.listarAlmacenes(req).then(
+      (success) => {
+        res.json(success);
+      },
+      (error) => {
+        res.json(error);
+      }
+    );
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// AGREGAR POPULARIDAD PRODUCTOS - EXPORTANDO RUTA
+router.post('/agregarAlmacen', function(req, res, next) {
+  try {
+    //web service
+    almacenesModelo.agregarAlmacen(req).then(
+      (success) => {
+        res.json(success);
+      },
+      (error) => {
+        res.json(error);
+      }
+    );
+  } catch (error) {
+    return next(error);
+  }
+});
+
+
+// ACTUALIZAR POPULARIDAD PRODUCTOS - EXPORTANDO RUTA
+router.put('/actualizarAlmacen/:idAlmacen', function(req, res, next) {
+  try {
+    //web service
+    almacenesModelo.actualizarAlmacen(req).then(
+      (success) => {
+        res.json(success);
+      },
+      (error) => {
+        res.json(error);
+      }
+    );
+  } catch (error) {
+    return next(error);
+  }
+});
+
+// ELIMINAR POPULARIDAD PRODUCTOS - EXPORTANDO RUTA
+router.delete('/eliminarAlmacen/:idAlmacen', function(req, res, next) {
+  try {
+    //web service
+    almacenesModelo.eliminarAlmacen(req).then(
+      (success) => {
+        res.json(success);
+      },
+      (error) => {
+        res.json(error);
+      }
+    );
+  } catch (error) {
+    return next(error);
+  }
+});
+
+
+module.exports = router;

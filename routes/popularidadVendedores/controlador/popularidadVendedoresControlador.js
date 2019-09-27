ce9bbf87bf952fd8c8bfdd393b77a7a0c8e43ce4
JLGallardoV <Gallardo.vaca@gmail.com>
2019-09-27 11:20:04 -0500

    APIs EL NUMERO VENTAS VENDEDOR / NUMERO DE VENTAS DE PRODUCTOS
    -Tiene unas breves condicionales para clasificar segun su venta NO esta completas
    -No JWT

---
+++ routes/popularidadVendedores/controlador/popularidadVendedoresControlador.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+var popularidadVendedoresModelo = require('../modelo/popularidadVendedoresModelo');
+
+
+// LISTAR POPULARIDAD PRODUCTOS - EXPORTANDO RUTA
+router.get('/listarPopularidadVendedores', function(req, res, next) {
+  try {
+    //web service
+    popularidadVendedoresModelo.listarPopularidadVendedores(req).then(
+      (success) => {
+        res.json(success);
+      },
+      (error) => {
+        res.json(error);
+      }
+    );
+  } catch (error) {
+    return next(error);
+  }
+});
+
+module.exports = router;

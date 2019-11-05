edfda6e12cb6263a77b72b01d488aaf80d72d1fd
JLGallardoV <Gallardo.vaca@gmail.com>
2019-11-05 12:48:55 -0600

    WS RENDIMIENTO VENDEDORES - MODIFICACADO
    - Se agrego la funcion de filtrar por fechas a este ws

--- routes/rendimientoVendedores/modelo/ventasVendedoresModelo.js
+++ routes/rendimientoVendedores/modelo/ventasVendedoresModelo.js
@@ -3,6 +3,9 @@ exports.listarVentasVendedores = function(req) {
   console.log("listando...");
   //regresaremos una promesa...
   return new Promise((resolve, reject) => {
+    //parametros a recibir
+    let inicioFechaTransacciones = req.params.inicioFechaTransacciones;
+    let finalFechaTransacciones = req.params.finalFechaTransacciones;
     //conectar con la base de datos
     req.getConnection(function(error, database) {
       if (error) {
@@ -11,8 +14,12 @@ exports.listarVentasVendedores = function(req) {
           respuesta: error
         });
       } else {
-        //CUANTAS VENTAS HIZO TAL VENDEDOR
-        var query = 'SELECT transacciones.idTransaccion, vendedores.nombreVendedor, count(*) AS vendidos FROM transacciones INNER JOIN vendedores ON transacciones.idVendedor = vendedores.idVendedor group by nombreVendedor order by vendidos DESC;';
+        //cuantas ventas hizo el vendedor en determinadas fechas
+        var query = `SELECT vendedores.nombreVendedor, count(*) AS vendidos
+                     FROM transacciones
+                     INNER JOIN vendedores ON transacciones.idVendedor = vendedores.idVendedor
+                     WHERE fechaTransaccion between '${inicioFechaTransacciones} 00:00:00' AND '${finalFechaTransacciones} 23:59:59
+                     group by nombreVendedor order by vendidos DESC';`;
 
         //ejecutamos el query
         database.query(query, function(error, success) {
